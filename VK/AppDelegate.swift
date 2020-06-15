02f757e530a9de6874308183c9b2bc79f07d8b0b
nikitatroin <nikitatroin@yandex.ru>
2020-06-15 19:10:09 +0300

    Create AppDelegate.swift

---
+++ VK/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  VK
+//
+//  Created by Никита Троян on 14.06.2020.
+//  Copyright © 2020 Никита Троян. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
